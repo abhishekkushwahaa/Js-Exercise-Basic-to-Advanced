c89d2c9572fd68255da63b4d3de386765f2f48e9
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2023-09-24 09:17:21 +0530

    04.js

--- 01_Basic/04.js
+++ 01_Basic/04.js
@@ -1,2 +1,9 @@
 // Create an array and access its elements.
 
+let arr = [1, 2, 3, 4, 5];
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
